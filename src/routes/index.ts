4b8e0427f860e7bce41111ab6f482f779bb36fd8
Rafael Farias Lima <finha.farias@outlook.com>
2020-09-25 15:13:49 -0300

    refs #1 create the base of application

---
+++ src/routes/index.ts
@@ -0,0 +1,9 @@
+import { response, Router } from "express";
+
+const routes = Router();
+
+routes.get("/", (req, res) => {
+  return res.json({ message: "Hello World" });
+});
+
+export default routes;
